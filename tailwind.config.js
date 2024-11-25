72c69ea1a3a10abd0d1709f3b19233efe14b4ce5
Yugh Juneja <yughjuneja@Yughs-MacBook-Pro.local>
2024-11-25 22:34:52 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['/Users/yughjuneja/Documents/land-registry-dapp/src/App.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
